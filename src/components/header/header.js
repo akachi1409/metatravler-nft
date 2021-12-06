af4e14596a5d5bd0611047f217e5de432f16ce54
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-07 07:30:13 +0900

    update the mute

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ class Header extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            mute:"Mute"
+            mute:"UnMute"
         }
     }
     onMute = () =>{
